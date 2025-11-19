ff06f8e094a92c5394383a42289165439c347d55
Agramm18 <grammaleks@gmail.com>
2025-11-19 20:54:33 +0100

    Added comments to describe functions

--- include/menuMain/menuArchitecture.h
+++ include/menuMain/menuArchitecture.h
@@ -4,12 +4,13 @@
 class MainMenue {
     public:
 
+    //Global class variables
     bool isChoiceValid = true; //For menu switch
     int menuChoice = 0;
     
-    void showMenu();
-    void getMenuChoice();
-    void validateMenuChoice();
+    void showMenu(); //Display the menu
+    void getMenuChoice(); //Get the menu choice from user
+    void validateMenuChoice(); //Validate the user choice with switch()
 
 };
 
