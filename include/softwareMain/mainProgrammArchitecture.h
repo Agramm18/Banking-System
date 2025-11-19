c52321975a7b9d6c8efc58a2c82cb2b33d9d550f
Agramm18 <grammaleks@gmail.com>
2025-11-19 20:55:35 +0100

    Added comments to describe functions

---
+++ include/softwareMain/mainProgrammArchitecture.h
@@ -0,0 +1,14 @@
+#ifndef MainLogic_H
+#define MainLogic_H
+
+//Define Main Programm Logic for the int main()
+
+class MainLogic {
+    public:
+
+    void StartingLogic(); //Define Starting Logic
+    void MainPorgrammLogic(); //Define Main Logic
+    void QuitLogic(); //Define Quitting Logic
+};
+
+#endif
