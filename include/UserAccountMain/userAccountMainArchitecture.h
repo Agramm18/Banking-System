f4a70086b23ec908e7ed6b7d6a02054f4d92e25d
Alex Gramm <alexg@retarus.com>
2025-11-25 16:49:02 +0100

    Added structure how user and card will be created

---
+++ include/UserAccountMain/userAccountMainArchitecture.h
@@ -0,0 +1,41 @@
+#ifndef userAccountMainArchitecture_H
+#define userAccountMainArchitecture_H
+
+//Define the main structure how your account and card will be created
+
+class createUser {
+    public:
+
+    //Create the user
+    void setUserName(); //Set the name from the user
+    void setBirthday(); //Set the birthday from the user
+    void setCardAgeType(); //Validate if its an adult or child bank card
+
+    void setEmail(); //Sets your E-Mail Adress
+    void setPhone(); //Void sets your phone number
+    
+    void generateUserID(); //Generates your User ID
+};
+
+class generateBankingCard {
+    public:
+
+    //Generates your Banking Card
+
+    void setBankName(); //Sets the name from the bank
+    void setCardType(); //Choose the card type vis, giro usw.
+    void setCardStatus(); //Set the card status and if its active or not
+
+    void generateBIC(); //Generate your BIC
+    void generateAccountNumber(); //Sets the Number from your Card Account
+    void generateBLZ(); //Generate the BLZ
+    void generateCVV(); //Generate Secret Number (only by credit cards)
+    void setExpirationDate(); //Set the expiration date from your card
+    
+    void generateIBAN(); //Generate your IBAN
+
+    void setPIN(); //Sets your PIN
+    void setPUK(); //Set your PUK
+};
+
+#endif
