a0c9861d66fcf69500043813a1bad48505480d05
Alex <grammaleks@gmail.com>
2025-12-04 18:47:51 +0100

    Added better controller

---
+++ include/ProgrammController/controller.h
@@ -0,0 +1,17 @@
+#ifndef controller.h
+#define controller.h
+
+#include <iostream>
+#include <string>
+
+//Defines the main running logic
+
+class ApplicationController {
+    
+    public:
+    void displayHelloMSG();
+    void run();
+
+};
+
+#endif
