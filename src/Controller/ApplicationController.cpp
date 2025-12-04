37f39b1dce75f1cd76e552e7a2de9116374e2da1
Alex <grammaleks@gmail.com>
2025-12-04 19:04:19 +0100

    Added controller file for programm

---
+++ src/Controller/ApplicationController.cpp
@@ -0,0 +1,28 @@
+#include <ProgrammController/controller.h>
+#include <MenuLogic/menuLogic.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void ApplicationController::displayHelloMSG() {
+    cout << "\n";
+    cout << "****************************************************\n";
+    cout << "*                                                  *\n";
+    cout << "*               WELCOME TO AGRAMM BANK             *\n";
+    cout << "*                                                  *\n";
+    cout << "****************************************************\n\n";
+
+    cout << "A C++ training project simulating a banking system.\n";
+    cout << "This program was developed to practice basic C++ syntax,\n";
+    cout << "and to learn how classes, methods, and OOP work in C++.\n";
+    cout << "Learn. Practice. Build. Try Out & Enjoy!!.\n\n";
+}
+
+void ApplicationController::run() {
+
+    displayHelloMSG();
+
+    MainMenuLogic mainMenu;
+    mainMenu.validateMenuChoice();
+}
